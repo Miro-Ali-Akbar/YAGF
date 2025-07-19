3356e46812e8b8d1e9c70d40da927fd7acb50b62
Miro Ali Akbar <Miro-Ali-Akbar@github.com>
2025-07-19 23:59:59 +0200

    added gitcli for today actions

--- git_fill.sh
+++ git_fill.sh
@@ -3,10 +3,14 @@
 # ./git_fill.sh $(date -d "1 year ago" +%Y-%m-%d)
 repository_folder="commits/"
 filename="file.txt"
-file=$repository_folder$filename
 text="Lorem ipsum"
+issue_title="Issues"
+branch_name="Pull_request"
 current_date=$(date -d "${1:-1 day ago}" +%Y-%m-%d)
 today=$(date +%Y-%m-%d)
+
+# INIT
+# push to a repo on gh
 
 if [ ! -d $repository_folder ]; then
   mkdir $repository_folder
@@ -14,6 +18,7 @@ if [ ! -d $repository_folder ]; then
   echo "Creating folder"
 fi
 
+cd $repository_folder
 while [[ "$current_date" < "$today" || "$current_date" == "$today"  ]]; do
   # weekday=$(date -d "$current_date" +%u)
   # if [[ "$weekday" -eq 6 || "$weekday" -eq 7  ]]; then
@@ -22,21 +27,41 @@ while [[ "$current_date" < "$today" || "$current_date" == "$today"  ]]; do
     number_of_commits=$((RANDOM % 10 + 1))
     for ((commit = 1; commit <= $number_of_commits; commit++)); do
 
-      if [[ ! -f $file ]]; then
-        echo $text > $file
-        git -C $repository_folder add $filename
+      if [[ ! -f $filename ]]; then
+        echo $text > $filename
+        git add $filename
       else
-        rm $file
-        git -C $repository_folder add $filename
+        rm $filename
+        git add $filename
       fi
 
-      commit_time="${current_date}T12:$(printf "%02d" "$commit"):00"
-      GIT_AUTHOR_DATE="$commit_time" \
-        GIT_COMMITTER_DATE="$commit_time" \
-        git -C $repository_folder commit -m "$commit commit for $current_date"
-      done
+      if [[ "$current_date" == "$today" ]]; then
+        issue1=$(gh issue create --title "$issue_title $commit.1" --body "$commit.1 comment for $current_date" | grep -oE "[0-9]+$")
+        issue2=$(gh issue create --title "$issue_title $commit.2" --body "$commit.2 comment for $current_date" | grep -oE "[0-9]+$")
+        gh issue close $issue1
+        gh issue close $issue2
+
+        git checkout -b "$branch_name"
+
+        commit_time="${current_date}T12:$(printf "%02d" "$commit"):00"
+        GIT_AUTHOR_DATE="$commit_time" \
+          GIT_COMMITTER_DATE="$commit_time" \
+          git commit -m "$commit commit for $current_date"
+        git push
+
+        pr1=$(gh pr create --title $current_date.$commit --body "$commit pull request for $current_date" | grep -oE "[0-9]+$")
+        gh pr merge -dm $pr1
+
+      else
+        commit_time="${current_date}T12:$(printf "%02d" "$commit"):00"
+        GIT_AUTHOR_DATE="$commit_time" \
+          GIT_COMMITTER_DATE="$commit_time" \
+          git commit -m "$commit commit for $current_date"
+
+      fi
+    done
   fi
 
   current_date=$(date -I -d "$current_date + 1 day")
 done
-git -C $repository_folder push
+git push
