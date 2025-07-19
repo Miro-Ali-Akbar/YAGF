c4294918e1ac23cb712c69f0137a5e6d66261940
Miro Ali Akbar <Miro-Ali-Akbar@github.com>
2025-07-20 00:22:21 +0200

    bit better code

--- git_fill.sh
+++ git_fill.sh
@@ -12,6 +12,24 @@ today=$(date +%Y-%m-%d)
 # INIT
 # push to a repo on gh
 
+
+file_changer() {
+  if [[ ! -f $filename ]]; then
+    echo $text > $filename
+    git add $filename
+  else
+    rm $filename
+    git add $filename
+  fi
+}
+
+commiter() {
+  commit_time="${current_date}T12:$(printf "%02d" "$commit"):00"
+  GIT_AUTHOR_DATE="$commit_time" \
+    GIT_COMMITTER_DATE="$commit_time" \
+    git commit -m "$commit commit for $current_date"
+  }
+
 if [ ! -d $repository_folder ]; then
   mkdir $repository_folder
   git -C $repository_folder init
@@ -20,6 +38,32 @@ fi
 
 cd $repository_folder
 while [[ "$current_date" < "$today" || "$current_date" == "$today"  ]]; do
+
+  if [[ "$current_date" == "$today" ]]; then
+
+    file_changer
+
+    issue1=$(gh issue create --title "$issue_title 1" --body "1 comment for $current_date" | grep -oE "[0-9]+$")
+    issue2=$(gh issue create --title "$issue_title 2" --body "2 comment for $current_date" | grep -oE "[0-9]+$")
+    gh issue close $issue1
+    gh issue close $issue2
+
+    git checkout -b "$branch_name"
+    commiter
+    git push
+
+    pr1=$(gh pr create --title $current_date --body "Pull request for $current_date" | grep -oE "[0-9]+$")
+    gh pr review --comment --body "comment 1 for pr"
+    gh pr review --comment --body "comment 2 for pr"
+    gh pr review --comment --body "comment 3 for pr"
+    # doesnt allow yourself to approve TODO:
+    # gh pr review --approve
+    gh pr merge -dm $pr1
+    echo "DONE!"
+    exit 0
+  fi
+
+  # TODO: weekday hack
   # weekday=$(date -d "$current_date" +%u)
   # if [[ "$weekday" -eq 6 || "$weekday" -eq 7  ]]; then
   if (( $RANDOM % 7 != 1)); then
@@ -27,44 +71,10 @@ while [[ "$current_date" < "$today" || "$current_date" == "$today"  ]]; do
     number_of_commits=$((RANDOM % 10 + 1))
     for ((commit = 1; commit <= $number_of_commits; commit++)); do
 
-      if [[ ! -f $filename ]]; then
-        echo $text > $filename
-        git add $filename
-      else
-        rm $filename
-        git add $filename
-      fi
+      file_changer
+      commiter
 
-      if [[ "$current_date" == "$today" ]]; then
-        issue1=$(gh issue create --title "$issue_title $commit.1" --body "$commit.1 comment for $current_date" | grep -oE "[0-9]+$")
-        issue2=$(gh issue create --title "$issue_title $commit.2" --body "$commit.2 comment for $current_date" | grep -oE "[0-9]+$")
-        gh issue close $issue1
-        gh issue close $issue2
-
-        git checkout -b "$branch_name"
-
-        commit_time="${current_date}T12:$(printf "%02d" "$commit"):00"
-        GIT_AUTHOR_DATE="$commit_time" \
-          GIT_COMMITTER_DATE="$commit_time" \
-          git commit -m "$commit commit for $current_date"
-        git push
-
-        pr1=$(gh pr create --title $current_date.$commit --body "$commit pull request for $current_date" | grep -oE "[0-9]+$")
-        gh pr review --comment --body "comment 1 for pr"
-        gh pr review --comment --body "comment 2 for pr"
-        gh pr review --comment --body "comment 3 for pr"
-        # doesnt allow yourself to approve TODO:
-        # gh pr review --approve
-        gh pr merge -dm $pr1
-
-      else
-        commit_time="${current_date}T12:$(printf "%02d" "$commit"):00"
-        GIT_AUTHOR_DATE="$commit_time" \
-          GIT_COMMITTER_DATE="$commit_time" \
-          git commit -m "$commit commit for $current_date"
-
-      fi
-    done
+      done
   fi
 
   current_date=$(date -I -d "$current_date + 1 day")
