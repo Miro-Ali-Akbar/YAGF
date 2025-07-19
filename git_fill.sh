47396b8f787d2658cfc4ec3417ecd725b0fe837c
Miro Ali Akbar <Miro-Ali-Akbar@github.com>
2025-07-19 23:13:36 +0200

    fixed so it should actually work now

--- git_fill.sh
+++ git_fill.sh
@@ -2,8 +2,17 @@
 # example
 # ./git_fill.sh $(date -d "1 year ago" +%Y-%m-%d)
 repository_folder="commits/"
+filename="file.txt"
+file=$repository_folder$filename
+text="Lorem ipsum"
 current_date=$(date -d "${1:-1 day ago}" +%Y-%m-%d)
 today=$(date +%Y-%m-%d)
+
+if [ ! -d $repository_folder ]; then
+  mkdir $repository_folder
+  git -C $repository_folder init
+  echo "Creating folder"
+fi
 
 while [[ "$current_date" < "$today" || "$current_date" == "$today"  ]]; do
   # weekday=$(date -d "$current_date" +%u)
@@ -12,10 +21,19 @@ while [[ "$current_date" < "$today" || "$current_date" == "$today"  ]]; do
 
     number_of_commits=$((RANDOM % 10 + 1))
     for ((commit = 1; commit <= $number_of_commits; commit++)); do
+
+      if [[ ! -f $file ]]; then
+        echo $text > $file
+        git -C $repository_folder add $filename
+      else
+        rm $file
+        git -C $repository_folder add $filename
+      fi
+
       commit_time="${current_date}T12:$(printf "%02d" "$commit"):00"
       GIT_AUTHOR_DATE="$commit_time" \
         GIT_COMMITTER_DATE="$commit_time" \
-        git -C $repository_folder commit --allow-empty -m "$commit commit for $current_date"
+        git -C $repository_folder commit -m "$commit commit for $current_date"
       done
   fi
 
